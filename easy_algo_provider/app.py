6a85c28a51a14d216f2e3597ad8da99bda2f2080
tom.mansion <tomansion@yahoo.fr>
2024-11-12 10:39:41 +0100

    Added some lazy importing

--- easy_algo_provider/app.py
+++ easy_algo_provider/app.py
@@ -1,13 +1,13 @@
-import uvicorn
-from fastapi import FastAPI, HTTPException
-from typing import List, Dict, Any
-from easy_algo_provider.models.algorithm import Algorithm
-from fastapi.middleware.cors import CORSMiddleware
-
 APP_VERSION = "0.1.0"
 
 
 def start_api_server(provider, host, port):
+    import uvicorn
+    from fastapi import FastAPI, HTTPException
+    from typing import List, Dict, Any
+    from easy_algo_provider.models.algorithm import Algorithm
+    from fastapi.middleware.cors import CORSMiddleware
+
     app = FastAPI(
         title="Algo API",
         version=APP_VERSION,
